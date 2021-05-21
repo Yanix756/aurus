ed64672fb7577ed07899f5e85818e537e123591e
Yanix756 <80466272+Yanix756@users.noreply.github.com>
2021-05-21 15:49:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.menu__btn').on('click', function(){
+
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+
+    });
+
+
+  
+});
